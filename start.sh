2d417589f0c0ef67d6ea5c25b2a1421fa3fd8772
doanthuanthanh88 <doanthuanthanh88@gmail.com>
2018-01-11 21:28:07 +0700

    Update command

--- start.sh
+++ start.sh
@@ -1,7 +1,15 @@
-nohup node oauth/.build/index.js & > oauth/log.txt
-nohup node files/.build/index.js & > files/log.txt
-nohup node mail/.build/index.js & > mail/log.txt
-nohup node log/.build/index.js & > log/log.txt
-nohup node dynamic/.build/index.js & > dynamic/log.txt
-nohup node auto-script/.build/index.js & > auto-script/log.txt
-nohup node api-monitor/.build/index.js & > api-monitor/log.txt
+cd oauth
+nohup node . > log.txt &
+cd ../files
+nohup node . > log.txt &
+cd ../mail
+nohup node . > log.txt &
+cd ../log
+nohup node . > log.txt &
+cd ../dynamic
+nohup node . > log.txt &
+cd ../auto-script
+nohup node . > log.txt &
+cd ../api-monitor
+nohup node . > log.txt &
+
