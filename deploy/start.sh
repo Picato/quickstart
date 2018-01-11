eb0439e6c09be877fcad76bb3c9004b93f89ec84
thanhdt-viosoft <thanhdt@viosoft.com>
2018-01-11 17:40:40 +0700

    add start command

---
+++ deploy/start.sh
@@ -0,0 +1,7 @@
+nohup ../oauth/.build/index.js & > ../log.txt
+nohup ../files/.build/index.js & > ../log.txt
+nohup ../mail/.build/index.js & > ../log.txt
+nohup ../log/.build/index.js & > ../log.txt
+nohup ../dynamic/.build/index.js & > ../log.txt
+nohup ../auto-script/.build/index.js & > ../log.txt
+nohup ../api-monitor/.build/index.js & > ../log.txt
