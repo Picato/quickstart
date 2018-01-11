f35625f6cf371091ff4ae728cdbdf6b315544f2c
thanhdt-viosoft <thanhdt@viosoft.com>
2018-01-11 17:58:18 +0700

    Move file start

--- deploy/start.sh
+++
@@ -1,7 +0,0 @@
-nohup node ../oauth/.build/index.js & > ../log.txt
-nohup node ../files/.build/index.js & > ../log.txt
-nohup node ../mail/.build/index.js & > ../log.txt
-nohup node ../log/.build/index.js & > ../log.txt
-nohup node ../dynamic/.build/index.js & > ../log.txt
-nohup node ../auto-script/.build/index.js & > ../log.txt
-nohup node ../api-monitor/.build/index.js & > ../log.txt
---
+++ start.sh
@@ -0,0 +1,7 @@
+nohup node oauth/.build/index.js & > oauth/log.txt
+nohup node files/.build/index.js & > files/log.txt
+nohup node mail/.build/index.js & > mail/log.txt
+nohup node log/.build/index.js & > log/log.txt
+nohup node dynamic/.build/index.js & > dynamic/log.txt
+nohup node auto-script/.build/index.js & > auto-script/log.txt
+nohup node api-monitor/.build/index.js & > api-monitor/log.txt
