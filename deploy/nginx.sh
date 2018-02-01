5a96709da8a1507439acf29a01e1c014bdddb4cc
thanhdt-viosoft <thanhdt@viosoft.com>
2018-02-01 11:37:43 +0700

    Update libs

--- deploy/nginx.sh
+++
@@ -1,9 +0,0 @@
-sudo mkdir /etc/nginx/conf.d/micro-service/
-sudo cp -R ./nginx/* /etc/nginx/conf.d/
-sudo cp -R ../api-gateway/deploy/nginx/* /etc/nginx/conf.d/micro-service/
-sudo cp -R ../api-monitor/deploy/nginx/* /etc/nginx/conf.d/micro-service/
-sudo cp -R ../auto-script/deploy/nginx/* /etc/nginx/conf.d/micro-service/
-sudo cp -R ../files/deploy/nginx/* /etc/nginx/conf.d/micro-service/
-sudo cp -R ../mail/deploy/nginx/* /etc/nginx/conf.d/micro-service/
-sudo cp -R ../log/deploy/nginx/* /etc/nginx/conf.d/micro-service/
-sudo cp -R ../oauth/deploy/nginx/* /etc/nginx/conf.d/micro-service/
